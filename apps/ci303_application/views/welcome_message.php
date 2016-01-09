91e2ab787cc8bf9c1ea47d03c2b79768a3bd2e15
Muhammad Hari Suharto <muharihar@gmail.com>
2016-01-09 21:48:10 +0700

    Add Favicon
    Add Favicon

--- apps/ci303_application/views/welcome_message.php
+++ apps/ci303_application/views/welcome_message.php
@@ -5,7 +5,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
         <title>Welcome to <?php echo strtoupper(MULTISITE_DOMAIN); ?>!</title>
-
+        <link rel="apple-touch-icon" sizes="57x57" href="/all/assets/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/all/assets/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/all/assets/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/all/assets/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/all/assets/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/all/assets/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/all/assets/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/all/assets/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/all/assets/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/all/assets/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/all/assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/all/assets/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/all/assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/all/assets/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/all/assets/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        
 	<style type="text/css">
 
 	::selection { background-color: #E13300; color: white; }
