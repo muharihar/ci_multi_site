5b07444ebaaaea81da78dbd3bb2eadb47829a7fb
Muhammad Hari Suharto <muharihar@gmail.com>
2016-01-16 20:48:44 +0700

    Add ushe.omahijo.net style

--- multisite/all/templates/margo/views/template.php
+++ multisite/all/templates/margo/views/template.php
@@ -39,19 +39,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/animate.css" media="screen">
 
   <!-- Color CSS Styles  -->
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/jade.css" title="jade" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/red.css" title="red" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/green.css" title="green" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/blue.css" title="blue" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/beige.css" title="beige" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/cyan.css" title="cyan" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/orange.css" title="orange" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/peach.css" title="peach" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/pink.css" title="pink" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/purple.css" title="purple" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/sky-blue.css" title="sky-blue" media="screen" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/all/themes/margo/')?>/css/colors/yellow.css" title="yellow" media="screen" />
-
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('sites/'.MULTISITE_MAIN_DOMAIN.'/'.MULTISITE_SITE_DOMAIN.'/themes/margo/')?>/css/colors/site-css.css" title="site-css" media="screen" />
+ 
   <!-- Margo JS  -->
   <script type="text/javascript" src="<?php echo base_url('sites/all/themes/margo/')?>/js/jquery-2.1.4.min.js"></script>
   <script type="text/javascript" src="<?php echo base_url('sites/all/themes/margo/')?>/js/jquery.migrate.js"></script>
@@ -1493,109 +1482,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
   </div>
 
-  <!-- Style Switcher -->
-  <div class="switcher-box">
-    <a class="open-switcher show-switcher"><i class="fa fa-cog fa-2x"></i></a>
-    <h4>Style Switcher</h4>
-    <span>12 Predefined Color Skins</span>
-    <ul class="colors-list">
-      <li>
-        <a onClick="setActiveStyleSheet('blue'); return false;" title="Blue" class="blue"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('sky-blue'); return false;" title="Sky Blue" class="sky-blue"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('cyan'); return false;" title="Cyan" class="cyan"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('jade'); return false;" title="Jade" class="jade"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('green'); return false;" title="Green" class="green"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('purple'); return false;" title="Purple" class="purple"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('pink'); return false;" title="Pink" class="pink"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('red'); return false;" title="Red" class="red"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('orange'); return false;" title="Orange" class="orange"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('yellow'); return false;" title="Yellow" class="yellow"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('peach'); return false;" title="Peach" class="peach"></a>
-      </li>
-      <li>
-        <a onClick="setActiveStyleSheet('beige'); return false;" title="Biege" class="beige"></a>
-      </li>
-    </ul>
-    <span>Top Bar Color</span>
-    <select id="topbar-style" class="topbar-style">
-      <option value="1">Light (Default)</option>
-      <option value="2">Dark</option>
-      <option value="3">Color</option>
-    </select>
-    <span>Layout Style</span>
-    <select id="layout-style" class="layout-style">
-      <option value="1">Wide</option>
-      <option value="2">Boxed</option>
-    </select>
-    <span>Patterns for Boxed Version</span>
-    <ul class="bg-list">
-      <li>
-        <a href="#" class="bg1"></a>
-      </li>
-      <li>
-        <a href="#" class="bg2"></a>
-      </li>
-      <li>
-        <a href="#" class="bg3"></a>
-      </li>
-      <li>
-        <a href="#" class="bg4"></a>
-      </li>
-      <li>
-        <a href="#" class="bg5"></a>
-      </li>
-      <li>
-        <a href="#" class="bg6"></a>
-      </li>
-      <li>
-        <a href="#" class="bg7"></a>
-      </li>
-      <li>
-        <a href="#" class="bg8"></a>
-      </li>
-      <li>
-        <a href="#" class="bg9"></a>
-      </li>
-      <li>
-        <a href="#" class="bg10"></a>
-      </li>
-      <li>
-        <a href="#" class="bg11"></a>
-      </li>
-      <li>
-        <a href="#" class="bg12"></a>
-      </li>
-      <li>
-        <a href="#" class="bg13"></a>
-      </li>
-      <li>
-        <a href="#" class="bg14"></a>
-      </li>
-    </ul>
-  </div>
-
-  <script type="text/javascript" src="<?php echo base_url('sites/all/themes/margo/')?>/js/script.js"></script>
-
+  <script type="text/javascript" src="<?php echo base_url('sites/all/themes/margo/')?>/js/script.js"></script>  
 </body>
 
 </html>
